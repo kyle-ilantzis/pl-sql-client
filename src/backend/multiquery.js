0c0327d7835a7bd25b7926dfeaeea7a6b163dd86
Pier-Luc Caron St-Pierre <pierluc.caronstpierre@gmail.com>
2015-09-01 22:01:21 -0400

    changement au data structure

--- src/backend/multiquery.js
+++ src/backend/multiquery.js
@@ -7,62 +7,69 @@ let moment = require('moment');
 let MultiQuery = function() {};
 
 /**
- * @param command to execute
- * @param command.url[] to use to execute the query
- * @param command.query sql query to execute
- */
+* @param command to execute
+* @param command.urls[] to use to execute the query
+* @param command.query sql query to execute
+*/
 MultiQuery.prototype.query = function(command) {
   return new Promise(function(resolve, reject){
     let request_date = moment().unix();
 
-    let mock = {
-      "SELECT * FROM pet;": {
-        "1441149765": [
-          {
-            "url": "driver://localhost/test",
-            "execution_start_date": request_date + 1,
-            "execution_end_date": request_date + 2,
-            "fields": [
-              "name",
-              "owner",
-              "species",
-              "sex",
-              "birth",
-              "death"
-            ],
-            "values": {
-              "name": "my_name",
-              "owner": "my_owner",
-              "species": "my_species",
-              "sex": "m",
-              "birth": "2013-01-01T05:00:00.000Z",
-              "death": null
-            }
-          },
-          {
-            "url": "localhost2",
-            "execution_start_date": request_date + 3,
-            "execution_end_date": request_date + 5,
-            "fields": [
-              "name",
-              "owner",
-              "species",
-              "sex",
-              "birth",
-              "death"
-            ],
-            "values": {
-              "name": "my_name",
-              "owner": "my_owner",
-              "species": "my_species",
-              "sex": "m",
-              "birth": "2013-01-01T05:00:00.000Z",
-              "death": null
-            }
-          }
-        ]
+    command.urls.forEach(function(url){
+
+    });
+
+    let mock = [
+      {
+        "url": "driver://localhost/test",
+        "execution_start_date": request_date + 1,
+        "execution_end_date": request_date + 2,
+        "fields": [
+          "name",
+          "owner",
+          "species",
+          "sex",
+          "birth",
+          "death"
+        ],
+        "values": [{
+          "name": "my_name",
+          "owner": "my_owner",
+          "species": "my_species",
+          "sex": "m",
+          "birth": "2013-01-01T05:00:00.000Z",
+          "death": null
+        },{
+          "name": "my_name2",
+          "owner": "my_owner2",
+          "species": "my_species2",
+          "sex": "m",
+          "birth": "2013-01-01T05:00:00.000Z",
+          "death": null
+        }]
+      },
+      {
+        "url": "localhost2",
+        "execution_start_date": request_date + 3,
+        "execution_end_date": request_date + 5,
+        "fields": [
+          "name",
+          "owner",
+          "species",
+          "sex",
+          "birth",
+          "death"
+        ],
+        "values": [{
+          "name": "my_name",
+          "owner": "my_owner",
+          "species": "my_species",
+          "sex": "m",
+          "birth": "2013-01-01T05:00:00.000Z",
+          "death": null
+        }]
       }
-    };
+    ];
 
     resolve(mock);
   });
