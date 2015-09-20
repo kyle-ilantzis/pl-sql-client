04808ee1122412643f867a87a884c10f5b2ff73b
kyle-ilantzis <ilantzis.kyle@gmail.com>
2015-09-20 12:45:32 -0400

    style the CmdError

--- src/js/views/dbmultiquery/CmdError.js
+++ src/js/views/dbmultiquery/CmdError.js
@@ -1,9 +1,33 @@
 (function(pl) {
 	
+	var MultiQuery = require('./backend/multiquery.js');
+	
 	pl.CmdError = React.createClass({
-		
+				
 		render: function() {
-			return <p>CMD ERR: {(this.props.error.message)}</p>
+			
+			var that = this;
+			
+			console.log(MultiQuery.prototype.Errors);
+			
+			var getErrorMsg = function() {
+				
+				switch(that.props.error) {
+					
+					case MultiQuery.prototype.Errors.ERROR_QUERY_EMPTY:
+						return "Please enter a query command.";
+					
+					case MultiQuery.prototype.Errors.ERROR_NO_DATABASE:
+						return "Please add databases to query in the settings page.";
+				}
+			};
+			
+			return  <div className="CmdError">                
+                <div className="CmdErrorMsg">
+                    <span className="CmdErrorIcon"></span>
+                    {getErrorMsg()}
+                </div>
+			</div>;
 		}
 	});
 })(pl||{});
